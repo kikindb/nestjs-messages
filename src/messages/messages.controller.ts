238942b997a30af905a2268228ac176686b0a88f
Enrique <enriquedb2@gmail.com>
2021-11-10 18:36:22 -0600

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,30 @@
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { runInThisContext } from 'vm';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+
+  messagesService: MessagesService;
+
+  constructor() {
+    //DONT DO THIS IN REAL APP
+    this.messagesService = new MessagesService();
+  }
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: string) {
+    return this.messagesService.findOne(id);
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+}
